fd49aa5f49030ae17991353f2ec7c4859e8d00a8
Josh Rivera <beps@Mactop.local>
2024-02-22 16:37:56 -0600

    Created function for handling the bop of a mole, and added a ternary operator to display either Mole or EmptySlot. Fixes: Changed setState to useState, and added props to score to define variable

--- src/Mole.js
+++ src/Mole.js
@@ -3,7 +3,7 @@
 const Mole = (props) => {
     return(
         <div>
-            <h1>Mole</h1>
+            
         </div>
     )
 }
--- src/MoleContainer.js
+++ src/MoleContainer.js
@@ -1,11 +1,21 @@
+import React, {useState} from "react";
 import { Mole } from "./Mole";
 import { EmptySlot } from "./EmptySlot";
 
+
 const MoleContainer = (props) => {
+    let [mole, displayMole] = useState(false)
+
+    const handleBop = (e) => {
+        props.setScore(props.score + 1)
+        displayMole(false)
+    }
+
+    let moleDisplay = mole ? <Mole  /> : <EmptySlot />
+
     return(
         <div>
-            <h1>Mole Container</h1>
-            <Mole />
+            {moleDisplay}
         </div>
     )
 }
